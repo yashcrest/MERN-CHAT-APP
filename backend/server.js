f8747958c15def6a2fcee1a6e80f3e82edec7e29
Yash Shrestha <yashcrest@gmail.com>
2024-03-30 19:45:00 +1100

    adding google oauth

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,2 @@
-// will let google pick and choose the type of authentication
-/* 
-    this is boiler plate to authenticate with google api
-*/
-import { GoogleAuth } from "google-auth-library";
-
-async function main() {
-  const auth = new GoogleAuth({
-    scopes: "https://www.googleapis.com/auth/cloud-platform",
-  });
-  const client = await auth.getClient();
-  const projectId = await auth.getProjectId();
-  const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
-  const res = await client.request({ url });
-  console.log(res.data);
-}
-
-main().catch(console.error);
+const express = require("express");
+const app = express();
