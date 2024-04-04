05bd7158fae86adef6bc558342af94875cba6a12
Yash Shrestha <yashcrest@gmail.com>
2024-04-04 16:41:47 +1100

    formatting login and register pages

--- frontend/src/components/Pages/Login.jsx
+++ frontend/src/components/Pages/Login.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 const Login = () => {
   const [fields, setFields] = useState();
   return (
     <>
       <h1 className="text-5xl">Log In to your account</h1>
-      <form className="login-form flex flex-col items-center h-lvh">
-        <div className="flex flex-col items-center">
-          <label className="flex-col">Email address:</label>
+      <form className="login-form">
+        <div className="">
+          <label className="">Email address:</label>
           <input type="text" name="" />
           <label>Password:</label>
           <input type="password" name="" />
           <button>Log In</button>
+          {/* routing to register page with Link tag */}
+          <span>
+            <Link to="/register">Create an Account.</Link>
+          </span>
         </div>
       </form>
     </>
--- frontend/src/components/Pages/Register.jsx
+++ frontend/src/components/Pages/Register.jsx
@@ -5,22 +5,18 @@ const register = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
-        <form onSubmit={handleSubmit} className="flex flex-col">
+        <form onSubmit={handleSubmit} className="flex flex-col form-item">
           <h1 className="text-4xl">Register a new Account</h1>
           <label htmlFor="username">Username:</label>
-          <input type="text" className="border-2 border-grey-100" />
+          <input type="text" className="input" required />
           <label htmlFor="email">Email:</label>
-          <input type="email" />
+          <input type="email" className="input" required />
           <label htmlFor="email">Password:</label>
-          <input type="password" />
+          <input type="password" className="input" required />
           <label htmlFor="email">Confirm Password:</label>
-          <input type="password" />
+          <input type="password" className="input" required />
           {/* submit button */}
-          <input
-            className="mx-5 border text-2xl"
-            type="submit"
-            value="Sign Up"
-          />
+          <input className="input-btn" type="submit" value="Sign Up" />
         </form>
       </div>
     </>
