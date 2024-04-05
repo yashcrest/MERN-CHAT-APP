c00eb111bd5b2f3430be30bcebec232f9565c9aa
Yash Shrestha <yashcrest@gmail.com>
2024-04-05 21:29:01 +1100

    making redux store available globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+// making redux state available globally
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
