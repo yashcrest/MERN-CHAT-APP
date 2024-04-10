5f3ac06c31414623effcef4a4f557763b4db0b44
Yash Shrestha <yashcrest@gmail.com>
2024-04-11 09:51:11 +1000

    remove cache changes

--- backend/schemas/userDetails.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const userDetails = new Schema({
-  username: string,
-  email: string,
-  password: string,
-});
-
-module.exports = model("userDetails", userDetails);
