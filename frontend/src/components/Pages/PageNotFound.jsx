a239de33b80c0a4a9168e55bb846da48872f5973
yash shrestha <yashcrest@gmail.com>
2024-04-15 15:43:42 +1000

    re-formatting 404 page

--- frontend/src/components/Pages/PageNotFound.jsx
+++ frontend/src/components/Pages/PageNotFound.jsx
@@ -4,7 +4,7 @@ const PageNotFound = () => {
   return (
     <>
       <h1 className="text-center text-2xl">
-        Go to a valid endpoint, this page does NOT exist! :(
+        Go to a valid endpoint, this page does NOT exist!
       </h1>
     </>
   );
