55b2c71485a6bd8830f0e33b03270db3cd0702d7
yash shrestha <yashcrest@gmail.com>
2024-04-17 15:21:21 +1000

    Refactor routing logic in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./styles.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import {
   Login,
   Register,
