70692192ef4809eb4cfd6186342be5ea63600e87
yash shrestha <yashcrest@gmail.com>
2024-04-21 16:15:49 +1000

    adding concurrently for running both backend and frontend at one go.

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Project Structure:
 
 1. `frontend` React frontend with Vite
 2. `backend` Nodejs frontend with Socket.io
+
+**Running Project Locally**
+If you want to run both frontend and backend at one go
+
+1. from root `cd backend`
+2. `npm run dev` to start both backend and frontend at one go.
+
+Package that made this possible : `concurrently`
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // anytime we call /api to our backend the target is defined already from here.
+      "/api": {
+        target: "http://localhost:3000", //backend address
+        changeOrigin: true,
+      },
+    },
+  },
 });
