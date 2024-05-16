2596e474ab7968d83b26a2585fa705de5cc53492
yash shrestha <yashcrest@gmail.com>
2024-05-16 20:09:57 +1000

    refactor: Update noChatSelected variable in MessageContainer

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -4,7 +4,7 @@ import MessageInput from "./MessageInput";
 import { PiChatsDuotone } from "react-icons/pi";
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const noChatSelected = false;
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
