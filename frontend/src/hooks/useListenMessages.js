735a617f8ce10ff028f9bb68acf107af84f19543
Yash Shrestha <yashcrest@gmail.com>
2024-05-23 18:30:07 +1000

    adding useListenMessage hook

---
+++ frontend/src/hooks/useListenMessages.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+const useListenMessages = () => {
+  const dispatch = useDispatch();
+};
+
+export default useListenMessages;
