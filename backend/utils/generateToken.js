6ec250bd71f42bf38fc407648f99d94c6413f5c5
yash shrestha <yashcrest@gmail.com>
2024-05-29 23:18:31 +1000

    refactor: Update generateToken.js to set sameSite attribute to "none" for JWT cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,10 +6,12 @@ const generateToken = (res, userId) => {
     expiresIn: "30d",
   });
   console.log("token generated in backend:", token);
+
+  //sending back to frontend
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60, //30 days
   });
 };
