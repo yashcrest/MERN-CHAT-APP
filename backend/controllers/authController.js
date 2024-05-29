f987f59a61d5b56e3d2e5a80244e7f1622fea6fb
yash shrestha <yashcrest@gmail.com>
2024-05-30 09:16:10 +1000

    refactor: Remove console.log statements and add logging for frontend URL and test token in authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,6 +14,7 @@ import generateToken from "../utils/generateToken.js";
 const authUser = asyncHandler(async (req, res) => {
   //explaination above
   const { username, password } = req.body;
+  console.log("cookie send from frontend", req.cookies);
 
   //comparing the user details in DB
   const user = await userDetails.findOne({ username });
@@ -28,7 +29,6 @@ const authUser = asyncHandler(async (req, res) => {
       profilePic: user.profilePic,
     });
     console.log("user sent valid creds");
-    console.log("cookie send from frontend", req.cookies);
   } else {
     res.status(401);
     throw new Error("Invalid username or password");
