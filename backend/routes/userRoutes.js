54dadb476a9b9f12fd0db56c88d2f47efb480592
Yash Shrestha <yashcrest@gmail.com>
2024-06-02 13:39:38 +1000

    refactor: Remove authentication requirement for getting users in sidebar

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,5 +3,5 @@ import { protect } from "../middleware/authMiddleware.js";
 import { getUsersForSidebar } from "../controllers/userController.js";
 const router = express.Router();
 
-router.get("/", protect, getUsersForSidebar);
+router.get("/", getUsersForSidebar);
 export default router;
