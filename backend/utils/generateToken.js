45b61c488db9bdb195e873ae6b4769f6a44f2522
yash shrestha <yashcrest@gmail.com>
2024-06-02 20:04:15 +1000

    refactor: Update sameSite attribute in generateToken.js to "lax"

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   //sending back to frontend
   res.cookie("jwt", token, {
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "lax",
     secure: true,
     maxAge: 24 * 60 * 60, //30 days
   });
