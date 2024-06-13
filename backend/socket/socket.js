d9eed00e4ceacd2217387b493dfa50321f6f75a6
yash shrestha <yashcrest@gmail.com>
2024-06-13 16:10:49 +1000

    refactor: Update CORS origin in socket.js to include chatapp.yashshrestha.net

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -11,6 +11,7 @@ const io = new Server(server, {
     origin: "https://chatapp.yashshrestha.net",
     methods: ["GET", "POST"],
     credentials: true,
+    allowedHeaders: ["Access-Control-Allow-Credentials"],
   },
 });
 
