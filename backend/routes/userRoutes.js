74450d502b6bfebc3998de2b332ebb71abb34bf5
yash shrestha <yashcrest@gmail.com>
2025-01-16 12:16:28 +1100

    add init API structure w user and msg routes, models, and middleware

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
-import { getUsersForSidebar } from "../controllers/userController.js";
-const router = express.Router();
-
-router.get("/", protect, getUsersForSidebar);
-export default router;
